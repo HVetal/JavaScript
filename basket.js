fe5fc7872e203868d6d7477ba5bd81b5b42d4e21
hv2011@inbox.ru <hv2011@inbox.ru>
2021-12-07 17:32:55 +0300

    lesson 8 in work

--- basket.js
+++ basket.js
@@ -1,5 +1,5 @@
 'use strict';
-let product = 0;
+
 
 class Product {
   constructor(id, name, price, count) {
@@ -14,26 +14,26 @@ class Product {
    */
   getProductMarkup() {
     return `
-      <div class="product">
+      <div class="ProductMarkup">
+          <div>${this.id}</div>
           <div>${this.name}</div>
-        //   <img src="${this.count}" alt="">
-          <div>Цена: <span>${this.price}</span> р.</div>
-          <a href="https://example.com/producs/${this.id}">Подробнее</a>
+          <div>$${this.price}</div>
+          <div>${this.count}</div>
       </div>
     `;
   }
 }
 
-const products = {
-  productEls: [
-    new Product(1, "ELLERY X M'O CAPSULE 1", 12.00, "images/featured/1.jpg"),
-    new Product(2, "ELLERY X M'O CAPSULE 2", 22.00, "images/featured/2.jpg"),
-    new Product(3, "ELLERY X M'O CAPSULE 3", 32.00, "images/featured/3.jpg"),
-    new Product(4, "ELLERY X M'O CAPSULE 4", 42.00, "images/featured/4.jpg"),
-    new Product(5, "ELLERY X M'O CAPSULE 5", 52.00, "images/featured/5.jpg"),
-    new Product(7, "ELLERY X M'O CAPSULE 6", 62.00, "images/featured/6.jpg"),
-  ]
-};
+// const products = {
+//   productEls: [
+//     new Product(1, "ELLERY X M'O CAPSULE 1", 12.00, "images/featured/1.jpg"),
+//     new Product(2, "ELLERY X M'O CAPSULE 2", 22.00, "images/featured/2.jpg"),
+//     new Product(3, "ELLERY X M'O CAPSULE 3", 32.00, "images/featured/3.jpg"),
+//     new Product(4, "ELLERY X M'O CAPSULE 4", 42.00, "images/featured/4.jpg"),
+//     new Product(5, "ELLERY X M'O CAPSULE 5", 52.00, "images/featured/5.jpg"),
+//     new Product(7, "ELLERY X M'O CAPSULE 6", 62.00, "images/featured/6.jpg"),
+//   ]
+// };
 
 //   const productsEl = document.querySelector('.products');
 //   document.querySelector('header').addEventListener('click', event => {
@@ -49,23 +49,61 @@ const products = {
 //   if (!event.target) {
 //     return;
 //   }
-document.querySelector('.featuredItems').addEventListener('click', event => {
-  if (event.target.tagName !== "BUTTON") {
-    return;
-  }
-  document.querySelector('.basketCount').textContent = ++product;
+// document.querySelector('.featuredItems').addEventListener('click', event => {
+//   if (event.target.classList.contains(".addToCart")) {
+//     return;
+//   }
+//   document.querySelector('.basketCount').textContent = ++product;
   // for (const attr of event.target.parentElement.parentElement.parentElement.dataset) {
   //   console.log(attr);
   // }
-  const attr = event.target.parentElement.parentElement.parentElement.dataset;
-  addToCartFunc(attr.id, attr.name, attr.price, attr.count);
-  //console.log(event.target.parentElement.parentElement.parentElement.dataset);
-});
+  //const attr = document.querySelectorAll('.featuredItem')
+  const parents = document.querySelectorAll('.featuredItem');
+  const sum = 0;
+  let productCart = 0;
+  let products = [];
+  for (let i = 0, parent; parent = parents[i]; i++) {
+    parent.addEventListener('click', event => {
+      if (event.target.className === 'addToCart') {
+        document.querySelector('.basketCount').textContent = ++productCart;
+        parent.dataset.count = 1;
+        //sum = parent.dataset.price * count;
+        addToCartFunc(parent.dataset.id, parent.dataset.name, parent.dataset.price, parent.dataset.count, productCart);
 
-function addToCartFunc(id, name, price, count) {
 
-  console.log(id);
+        //console.log(parent.dataset);
+      }
+    });
+  }
+  const cartWindow = document.querySelector('.popupBasket');
+function addToCartFunc(id, name, price, count, i) {
+  products[i] = new Product(id, name, price, count); 
+  cartWindow.innerHTML += products[i].getProductMarkup();
+  //console.log(products);
 }
+
+//console.log(cartWindow);
+// for (const el of products) {
+//     cartWindow.innertHTML = products.el;
+//    }
+
+
+// const cartWindow = document.querySelector('.featuredTitle');
+// cartWindow.insertAdjacentText = products[1]["name"];
+ function print (products) {
+ for (let key in products) {
+    return products.map(product => product.getProductMarkup());
+    // console.log(products[key]);
+// return key.map(product => product.getProductMarkup());
+}}
+//   const attr = event.target.classList.contains(".featuredItem").dataset;
+//   addToCartFunc(attr.id, attr.name, attr.price, attr.count);
+// });
+
+// function addToCartFunc(id, name, price, count) {
+
+//   console.log(id);
+//}
 
 // }));
 
